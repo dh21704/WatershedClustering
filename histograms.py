9737e6df09286562295b4f342790257290bc9b0c
Daniel Hernandez <danielhernandez.gk@gmail.com>
2024-08-11 16:54:20 -0700

    updating hisograms.py, need to fix the function that was added and removed aquifer

--- histograms.py
+++ histograms.py
@@ -9,29 +9,34 @@ print(df.info())
 bins = [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000]
 
 
+def show_historgram(df, title_name, xlabel, ylabel, title):
+    data = df[title_name]
+
+    plt.figure(figsize=(10,6))
+    plt.hist(df, bins=bins, edgecolor='black')
+    plt.title(title, size=15)
+    plt.xlabel(xlabel, size=15)
+    plt.ylabel(ylabel, size=15)
+    plt.tight_layout()
+    plt.show()
+
+
+show_historgram(df, 'Annual Avg. Snow', 'Snow (mm)', 'Frequency', 'Annual Avg. Snow')
 #plot streamflow
-plt.hist(df['Annual Avg. River Flow'], bins = bins, edgecolor='black', color='purple')
-plt.title('Annual Avg. River Flow')
-plt.xlabel('River Flow (mm)')
-plt.ylabel('Number of River Flow Respondents')
+plt.hist(df['Annual Avg. River Flow'], bins = bins, edgecolor='black')
+plt.title('Annual Avg. River Flow', size=15)
+plt.xlabel('River Flow (mm)', size=15)
+plt.ylabel('Frequncy', size=15)
 plt.tight_layout()
 plt.show()
 
-#########################################################################
-#plot recharge
-plt.hist(df['Annual Avg. Aquifer Recharge'], bins=bins, edgecolor='black')
-plt.title('Annual Avg. Aquifer Recharge')
-plt.xlabel('Aquifer Recharge (mm)')
-plt.ylabel('Number of Aquifer Recharge Respondents')
-plt.tight_layout()
-plt.show()
 
 #########################################################################
 #plot precipitation
 plt.hist(df['Annual Avg. Precipitation'], bins=bins, edgecolor='black')
-plt.title('Annual Avg. Precipitation')
-plt.xlabel('Annual Avg. Precipitation (mm)')
-plt.ylabel('Number of Precipitation Respondents')
+plt.title('Annual Avg. Precipitation', size=15)
+plt.xlabel('Precipitation (mm)', size=15)
+plt.ylabel('Frequency', size=15)
 plt.tight_layout()
 plt.show()
 
@@ -41,5 +46,27 @@ plt.hist(df['Annual Avg. Evapotranspiration'], bins=bins, edgecolor='black')
 plt.title('Annual Avg. Evapotranspiration')
 plt.xlabel('Evapotranspiration (mm)')
 plt.ylabel('Number of Evapotranspiration Respondents')
+plt.tight_layout()
+plt.show()
+
+#########################################################################
+bins2 = [50, 100, 150, 200, 250, 300, 350, 400, 450]
+
+#plot snow
+plt.hist(df['Annual Avg. Snow'], bins=bins2, edgecolor='black')
+plt.title('Annual Avg. Snow')
+plt.xlabel('Snow (mm)')
+plt.ylabel('Frequency')
+plt.tight_layout()
+plt.show()
+
+#########################################################################
+bins2 = [50, 100, 150, 200, 250, 300, 350, 400, 450]
+
+#plot snow
+plt.hist(df['Annual Avg. Snow'], bins=bins2, edgecolor='black')
+plt.title('Annual Avg. Snow', size=15)
+plt.xlabel('Snow (mm)', size=15)
+plt.ylabel('Frequency', size=15)
 plt.tight_layout()
 plt.show()
